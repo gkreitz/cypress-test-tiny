0ade02973b14966efed31daee8f8efe1c0510fd0
Gunnar Kreitz <gkreitz@kattis.com>
2023-06-09 10:49:07 +0200

    Add debug logging to cypress run command

--- run_cypress.sh
+++ run_cypress.sh
@@ -2,4 +2,4 @@
 
 TESTDIR=$(readlink -f $(dirname $0))
 
-docker run --net=host --ipc=host --rm -v $TESTDIR:/tests -w /tests cypress/included:12.4.0
+docker run --net=host --ipc=host --rm -v $TESTDIR:/tests -w /tests -e "DEBUG=cypress:*" cypress/included:12.4.0
